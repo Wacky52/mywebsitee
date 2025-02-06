ad220f98c79771f1e34c9324805808c20f9e8398
wacky <wackyislucky@gmail.com>
2025-02-06 21:01:42 +0500

    Version с ошибкой

--- home/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from home.models import  Setting
-class SettingAdmin(admin.ModelAdmin):
-    list_display = ['title']
-
-
-admin.site.register(Setting, SettingAdmin)
